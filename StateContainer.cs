856df540f27f443f62e62d4677383d7c895877f4
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-11-15 20:53:34 +0200

    Update NuGet

--- StateContainer.cs
+++ StateContainer.cs
@@ -3,11 +3,23 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Arora.Blazor.StateContainer;
 public class StateContainer
 {
+    /// <summary>
+    /// Occurs when the state has changed.
+    /// Use += StateHasChanged to subscribe to this event.
+    /// </summary>
     public event Action? OnChange;
+    /// <summary>
+    /// Notifies the state container that the state has changed.
+    /// </summary>
     public void NotifyStateChanged() => OnChange?.Invoke();
 }
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Injects a StateContainer into the DI container as a scoped service.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static IServiceCollection AddStateContainer(this IServiceCollection services) =>
         services.AddScoped<StateContainer>();
 }
